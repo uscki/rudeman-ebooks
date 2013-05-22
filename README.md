feaa4063c1a2c8938b448bc76dd4810df1a87ab5
Benno Kruit <bennokr@gmail.com>
2013-05-23 00:04:11 +0200

    Leuk scriptje

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ rudeman-ebooks
 ==============
 
 Een hidden markov model twitter bot als grens van het universum
+
+Benodigdheden
+-------------
+
+- Python (http://python.org/)
+- python-twitter (https://github.com/bear/python-twitter)
+- Twitter API toegang (https://dev.twitter.com/apps) en 4 OAuth codes:
+
+> CONSUMER_KEY    = 'je-consumer-key'
+> CONSUMER_SECRET = 'je-consumer-secret'
+> ACCESS_KEY      = 'een-access-key'
+> ACCESS_SECRET   = 'een-access-secret'
+
+
+Waarom?
+-------
+
+Dit is een heel simpel taalmodel waar iets minder onzinnige onzin uitkomt dat een volstrekt willekeurige. Maar dat ligt natuurlijk aan je corpus. Met python kan je makkelijk leuke kunstjes doen.
+
+Hoe?
+----
+
+Elk woord krijgt een lijstje met welke woorden er na voorkomen in het corpus. Hierbij worden dubbelen gewoon toegevoegd. Als je dan een wilekeurig woord uit dat rijtje pakt om er achter te plakken, krijg je een mooie waarschijnlijkheidsverdeling.
+Dat betekent dat de kans op een woord `a` na woord `b` is 0 als `a` nooit na `b` voorkomt in het corpus, en anders `het aantal keren dat a na b voorkomt / het aantal woorden dat na b komt`.
+(...)
+Grens van het universum.
---
+++ markov.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import random
+import time
+import os
+import re
+import twitter # from https://github.com/bear/python-twitter
+import twittersecrets # contains keys and secrets
+
+# NEEDED:
+# rudeman.txt
+# twittersecrets.py
+
+
+def style_dict(filename):
+    """Returns style_dict mapping each word to list of words which follow it.
+        Args:filename
+        Returns: dictionary of words with potential followings words
+    """
+    f = open(filename, 'r')
+    tekst = f.read().split()
+    word_dict = {}
+    word_dict[''] = [tekst[0]]
+    for i in range(len(tekst)-1):
+        w = tekst[i]
+        if w in word_dict.keys():
+            word_dict[w].append(tekst[i+1])
+        else:
+            word_dict[w] = [tekst[i+1]]
+        if list(w)[0] == '>':
+            word_dict[''].append(w)
+
+    return word_dict
+
+
+def print_speech(style_dict, word):
+    """Given style_dict and start word, prints 100 random words.
+        Args: Dictionary
+        Returns: prints to screen
+    """
+    text = []
+    for i in range(100):
+        if not word in style_dict.keys():
+            word = ''
+        new_word = random.choice(style_dict[word])
+        text.append(new_word)
+        word = new_word
+    return text
+
+
+def main():
+    """Rudeman-generator: Mimics style of Rudeman's forum posts.
+        Args: void
+        Returns : none
+    """
+    dict = style_dict('rudeman.txt')
+
+    api = twitter.Api(consumer_key          = twittersecrets.CONSUMER_KEY,
+                      consumer_secret       = twittersecrets.CONSUMER_SECRET,
+                      access_token_key      = twittersecrets.ACCESS_KEY,
+                      access_token_secret   = twittersecrets.ACCESS_SECRET)
+
+    while True: 
+
+        to_print = print_speech(dict, '') #Start from empty string
+        charlist = []
+        for w in to_print:
+            for c in w:
+                charlist.append(c)
+            charlist.append(' ')
+            if len(charlist) > 140:
+                break
+
+        i = 140
+        interpunction = False
+        while not interpunction and i > 0:
+            i -= 1
+            if charlist[i] in ['"','.','!','?']:
+                interpunction = True
+
+        if i > 5:
+            tweet = re.sub('>', '', ''.join(charlist[:i+1]))
+            api.PostUpdate(tweet)
+            print tweet
+            t = time.clock()
+            period = 43200
+            while True:
+                if (time.clock() - t > period):
+                    break
+
+
+if __name__ == "__main__":
+    main()
